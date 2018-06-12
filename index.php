96d8d31760f4c663cca4b6c44e1703531d6a4dbd
4fontz <4fontz@gmail.com>
2018-06-12 21:50:43 +0530

    commiting changes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(file_exists(dirname(__FILE__).'/protected/config/production.php')){
 	$config = dirname(__FILE__).'/protected/config/main.php';
 }
 
-// remove the following lines when in production mode
+/// remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
