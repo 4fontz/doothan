141e20194761b4e8af243f52a099bfa7bd23be5c
4fontz <4fontz@gmail.com>
2018-06-20 00:09:22 +0530

    changes

--- protected/views/layouts/header.php
+++ protected/views/layouts/header.php
@@ -12,7 +12,7 @@
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
-		<?php echo Yii::app()->user->getId();die;$loggedUserDetails = Admin::model()->findByPk(Yii::app()->user->getId());?>
+		<?php echo $loggedUserDetails = Admin::model()->findByPk(Yii::app()->user->getId());?>
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
