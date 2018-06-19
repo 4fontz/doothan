ca2c072747deaadd019d46b127dedb5b515bcd40
4fontz <4fontz@gmail.com>
2018-06-19 10:11:11 +0530

    activity log corrected

--- protected/views/activity/index.php
+++ protected/views/activity/index.php
@@ -18,7 +18,7 @@
        				$activity_date  = $mod->created_on;
                     $dt = new DateTime($activity_date);
                     $date = $dt->format('Y-M-d');
-                    $time = $dt->format('H:i A');?>
+                    $time = $dt->format('h:i A');?>
             <li class="time-label">
              <?php if($predate!==$mod->month_year_only){ ?>
                   <span class="bg-red">
@@ -30,7 +30,7 @@
               <?php if($mod->type=='REGISTER'){ ?>
                 <i class="fa fa-user bg-aqua"></i>
                 <div class="timeline-item">
-                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $time; ?></span>
+                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $date." ".$time; ?></span>
                 <h4 class="timeline-header"><a href="#">New User Registration</a></h4>
                 <div class="timeline-body">
                  <?php echo $mod->message; ?>
@@ -39,28 +39,28 @@
               <?php }else if($mod->type=='LOG IN'){ ?>
                 <i class="fa fa-unlock-alt bg-yellow"></i>
                 <div class="timeline-item">
-                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $time; ?></span>
+                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $date." ".$time; ?></span>
                 <div class="timeline-body"><?php echo $mod->message; ?></div>
                 
               </div>
               <?php }else if($mod->type=='LOG OUT'){ ?>
                 <i class="fa fa-unlock-alt bg-red"></i>
                 <div class="timeline-item">
-                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $time; ?></span>
+                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $date." ".$time; ?></span>
                 <div class="timeline-body"><?php echo $mod->message; ?></div>
                 
               </div>
               <?php }else if($mod->type=='Notification'){?>
                 <i class="fa fa-bell" aria-hidden="true"></i>
                 <div class="timeline-item">
-                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $time; ?></span>
+                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $date." ".$time; ?></span>
                 <div class="timeline-body"><?php echo $mod->message; ?></div>
                 
               </div>
               <?php }else{?> 
                 <i class="fa fa-envelope bg-blue"></i>
                 <div class="timeline-item">
-                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo  $time; ?></span>
+                <span class="time"><i class="fa fa-clock-o"></i> <?php  echo $date." ".$time; ?></span>
                 <h3 class="timeline-header"><a href="#">Support Team</a> Verification mail sent Successfully</h3>
                 <div class="timeline-body">
                  <?php echo $mod->message; echo $mod->type; ?>
